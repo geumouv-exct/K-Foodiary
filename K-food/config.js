1a01f5c9fdec196d0cc02b183fc1551873024582
ikkkki <163969326+geumouv-exct@users.noreply.github.com>
2026-05-27 02:50:02 +0900

    Delete K-food directory

--- K-food/config.js
+++
@@ -1,9 +0,0 @@
-// Google setup
-// 1) Create an OAuth Web Client ID in Google Cloud Console.
-// 2) Add your deployed domain to Authorized JavaScript origins.
-// 3) Paste the Client ID below.
-// For local Mac preview, the app provides a local-preview login when this is empty.
-window.KFOOD_CONFIG = {
-  GOOGLE_CLIENT_ID: "",
-  GOOGLE_MAPS_API_KEY: ""
-};
